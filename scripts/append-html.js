9f9b23cd30a524fb43691cc410deec96f4be6583
Forrest Thomas <forrestbthomas@gmail.com>
2018-01-23 09:56:05 -0800

    debug client side JS for archive post retrieval

--- scripts/append-html.js
+++ scripts/append-html.js
@@ -9,27 +9,29 @@ var styleToggle = {
 function reqListener (n) {
 	var t = document.createElement('template');
 	var p = n.parentNode;
+	p.removeChild(n);
 	t.innerHTML = this.responseText;
 	p.appendChild(t.content.firstChild);
-	p.lastChild.addEventListener("click", toggleView);
+	p.appendChild(n);
+	GLOBAL_SHOW_MORE_COUNTER--;
 }
-function handleError(n) {
-	var p = n.parentNode;
-	p.removeChild(n);
-}
-function handleSend(c) {
+function handleSend(n, c) {
 	c--;
 	this.send();
 }
-function fetch(n, c) {
-	try {
-		var oReq = new XMLHttpRequest();
-		var cs = c >= 0 ? c : GLOBAL_SHOW_MORE_COUNTER;
-		oReq.addEventListener("load", reqListener.bind(oReq, n));
-		oReq.open("GET", "/posts/" + cs  +".html");
-		cs < 0 ? handleError.call(oReq, n) : handleSend.call(oReq, cs);
-	} catch (e) {
-		console.log("error: ", e.message);
+function fetch(n) {
+	if (GLOBAL_SHOW_MORE_COUNTER >= 0) {
+		try {
+			var oReq = new XMLHttpRequest();
+			oReq.addEventListener("load", reqListener.bind(oReq, n));
+			oReq.open("GET", "/posts/" + GLOBAL_SHOW_MORE_COUNTER  +".html");
+			handleSend.call(oReq, n, GLOBAL_SHOW_MORE_COUNTER);
+		} catch (e) {
+			console.log("error: ", e.message);
+		}
+	} else {
+		var p = n.parentNode;
+		p.removeChild(n);
 	}
 }
 
--- server/inject-html.js
+++ server/inject-html.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var ON_LOAD_COUNTER = 2;
+var ON_LOAD_COUNTER = fs.readdirSync('./posts/').length - 1;
 var SHOW_MORE_COUNTER = (ON_LOAD_COUNTER - 3);
 var POST_SECTION = new RegExp("<!-- INJECT:HTML -->");
 var INDEX_HTML = fs.readFileSync('./placeholder.index.html').toString('utf8');
@@ -14,6 +14,7 @@ for (var i = 3; i > 0; i--) {
 if (SHOW_MORE_COUNTER >= 0) {
 	POSTS.push(showMore);
 }
+POSTS.push('<script>this.GLOBAL_SHOW_MORE_COUNTER='+SHOW_MORE_COUNTER+'</script>')
 
 var REPLACED_INDEX = INDEX_HTML.replace(POST_SECTION, POSTS.join(''));
 fs.writeFileSync('./index.html', REPLACED_INDEX)
