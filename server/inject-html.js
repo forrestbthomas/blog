6a58e47d7fb2f8823462a501fe08b876231cf230
Forrest Thomas <forrestbthomas@gmail.com>
2018-01-27 10:55:53 -0800

    use archive instead of dynamic loading;

--- server/inject-html.js
+++ server/inject-html.js
@@ -1,20 +1,21 @@
 var fs = require('fs');
 var ON_LOAD_COUNTER = fs.readdirSync('./posts/').length - 1;
-var SHOW_MORE_COUNTER = (ON_LOAD_COUNTER - 3);
 var POST_SECTION = new RegExp("<!-- INJECT:HTML -->");
 var INDEX_HTML = fs.readFileSync('./placeholder.index.html').toString('utf8');
+var ARCHIVE_HTML =  fs.readFileSync('./archive/placeholder.index.html').toString('utf8');
 var POSTS = [];
-var showMore = '<div class="clickable" onclick="fetch(this);">Show More...</div>';
+var ARCHIVE = [];
+console.log("POSTS: ", fs.readdirSync('./posts'))
 
 for (var i = 3; i > 0; i--) {
 	POSTS.push(fs.readFileSync('./posts/' + ON_LOAD_COUNTER + '.html').toString('utf8'));
 	ON_LOAD_COUNTER--;
 }
-
-if (SHOW_MORE_COUNTER >= 0) {
-	POSTS.push(showMore);
-}
-POSTS.push('<script>this.GLOBAL_SHOW_MORE_COUNTER='+SHOW_MORE_COUNTER+'</script>')
-
 var REPLACED_INDEX = INDEX_HTML.replace(POST_SECTION, POSTS.join(''));
 fs.writeFileSync('./index.html', REPLACED_INDEX)
+
+for (var i = 0; i <= ON_LOAD_COUNTER; i++) {
+	ARCHIVE.push(fs.readFileSync('./posts/' + ON_LOAD_COUNTER + '.html').toString('utf8'));
+}
+var ARCHIVE_REPLACED_INDEX = ARCHIVE_HTML.replace(POST_SECTION, ARCHIVE.join(''));
+fs.writeFileSync('./archive/index.html', ARCHIVE_REPLACED_INDEX);
